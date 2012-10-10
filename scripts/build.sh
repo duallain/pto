ee42f343f7937fa79fe3084e1d5d0ce2b63f0723
Peter Bengtsson <mail@peterbe.com>
2012-10-10 08:24:35 -0700

    also removed bad hashers from build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -34,18 +34,6 @@ DATABASES = {
 }
 SECRET_KEY = 'somethingaslongasitsnotblank'
 
-HMAC_KEYS = {
-    '2012-06-06': 'anything',
-}
-from django_sha2 import get_password_hashers
-hashers = (#'django_sha2.hashers.BcryptHMACCombinedPasswordVerifier',
- #'django_sha2.hashers.SHA512PasswordHasher',
- #'django_sha2.hashers.SHA256PasswordHasher',
- 'django.contrib.auth.hashers.SHA1PasswordHasher',
- 'django.contrib.auth.hashers.MD5PasswordHasher',
- 'django.contrib.auth.hashers.UnsaltedMD5PasswordHasher'
-)
-PASSWORD_HASHERS = get_password_hashers(hashers, HMAC_KEYS)
 AUTH_LDAP_BIND_PASSWORD = 'Anything'
 AUTH_LDAP_BIND_DN = 'Something'
 CACHES = {
